4ade9258d46f3f3f851f9d8615ddd1d2dd8032fb
Michael Fogleman <fogleman@gmail.com>
2021-01-20 11:40:26 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='sdf',
+    version='0.1',
+    description='Generate 3D meshes from signed distance functions.',
+    author='Michael Fogleman',
+    author_email='michael.fogleman@gmail.com',
+    packages=['sdf'],
+    install_requires=['numpy', 'scikit-image'],
+    license='MIT',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ),
+)
