1c9cd0ea2cd762f189f7cb6fbf5937944780a057
Michael Fogleman <fogleman@gmail.com>
2021-02-05 10:33:32 -0500

    bend_radial

--- sdf/d3.py
+++ sdf/d3.py
@@ -431,6 +431,18 @@ def bend(other, k):
     return f
 
 @op3
+def bend_radial(other, r0, r1, dz, e=ease.linear):
+    def f(p):
+        x = p[:,0]
+        y = p[:,1]
+        z = p[:,2]
+        r = np.hypot(x, y)
+        t = np.clip((r - r0) / (r1 - r0), 0, 1)
+        z = z + dz * e(t)
+        return other(_vec(x, y, z))
+    return f
+
+@op3
 def transition(f0, f1, p0=-Z, p1=Z, e=ease.linear):
     p0 = np.array(p0)
     p1 = np.array(p1)
