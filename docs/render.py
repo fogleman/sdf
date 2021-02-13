faba67a274e684c15fb2c498f8b9136e8a10229a
Michael Fogleman <fogleman@gmail.com>
2021-02-13 15:02:23 -0500

    images

--- docs/render.py
+++ docs/render.py
@@ -6,6 +6,8 @@ def generate(f, name, samples=2**26):
     os.makedirs('images', exist_ok=True)
     stl_path = 'models/%s.stl' % name
     png_path = 'images/%s.png' % name
+    if os.path.exists(png_path):
+        return
     render_cmd = './render %s %s' % (stl_path, png_path)
     f.save(stl_path, samples=samples)
     os.system(render_cmd)
@@ -16,13 +18,17 @@ c = cylinder(0.5)
 f -= c.orient(X) | c.orient(Y) | c.orient(Z)
 generate(f, 'example')
 
-# sphere
+# sphere(radius=1, center=ORIGIN)
 f = sphere(1)
 generate(f, 'sphere')
 
-# box
+# box(size=1, center=ORIGIN, a=None, b=None)
 f = box(1)
 generate(f, 'box')
 
 f = box((1, 2, 3))
 generate(f, 'box2')
+
+# rounded_box(size, radius)
+f = rounded_box((1, 2, 3), 0.25)
+generate(f, 'rounded_box')
