d3092b8f15cb7ab787af48ed923bf39426441db4
cactrot <jfields@mananhearing.com>
2022-12-15 16:16:25 -0500

    New version of mirror
    Rather than mirroring all axes at once this version should mirror about a plan in point and normal form.

--- sdf/d3.py
+++ sdf/d3.py
@@ -404,9 +404,16 @@ def elongate(other, size):
     return f
 
 @op3
-def mirror(other, offset):
+def mirror(other, point=ORIGIN, normal=UP ):
+    x, y, z = _normalize(normal)
+    
+    matrix = np.array([
+        [1-2*x*x,  -2*x*y,  -2*x*z],
+        [ -2*x*y, 1-2*y*y,  -2*y*z],
+        [ -2*x*z,  -2*y*z, 1-2*z*z],
+    ]).T
     def f(p):
-        return other(offset - p)
+        return other(np.dot(p-point, matrix) + point)
     return f
 
 @op3
