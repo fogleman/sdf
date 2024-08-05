facd9f649f612c238fb1fdda06569e63eef9cfdd
Michael Fogleman <fogleman@gmail.com>
2024-08-04 21:03:30 -0400

    better Mesh sampling

--- sdf/mesh.py
+++ sdf/mesh.py
@@ -1,12 +1,10 @@
+from scipy import interpolate
+
 import numpy as np
 import threading
 
 from .d3 import sdf3, box
 
-# Triangle Meshes
-
-# TODO: support linear transformations on point batches
-# TODO: make sure vdb grid aligns with sample grid
 # TODO: allow transforming mesh
 class Mesh:
     @classmethod
@@ -45,39 +43,46 @@ class Mesh:
         return Mesh(points, self.triangles)
 
     @sdf3
-    def sdf(self, half_width=None):
+    def sdf(self, voxel_size, half_width=None):
         import pyopenvdb as vdb
-
-        lock = threading.Lock()
-        grids = {}
-
-        def get_grid(step):
-            with lock:
-                if step not in grids:
-                    dx, dy, dz = step
-                    half_width_voxels = 3
-                    if half_width is not None:
-                        half_width_voxels = max(
-                            half_width_voxels, int(np.ceil(half_width / min(dx, dy, dz))))
-                    transform = vdb.createLinearTransform(
-                        [[dx, 0, 0, 0], [0, dy, 0, 0], [0, 0, dz, 0], [0, 0, 0, 1]])
-                    grid = vdb.FloatGrid.createLevelSetFromPolygons(
-                        self.points, triangles=self.triangles,
-                        transform=transform, halfWidth=half_width_voxels)
-                    grids[step] = grid
-                return grids[step]
 
         a, b = self.bounding_box
         estimator = box(a=a, b=b)
 
+        transform = vdb.createLinearTransform(voxelSize=voxel_size)
+
+        half_width_voxels = 3
+        if half_width is not None:
+            half_width_voxels = max(
+                half_width_voxels, int(np.ceil(half_width / voxel_size)))
+
+        grid = vdb.FloatGrid.createLevelSetFromPolygons(
+            self.points, triangles=self.triangles,
+            transform=transform, halfWidth=half_width_voxels)
+
+        v0, v1 = grid.evalActiveVoxelBoundingBox()
+        ijk0 = np.array(v0, dtype=int)
+        ijk1 = np.array(v1, dtype=int)
+        size = ijk1 - ijk0 + 1
+
+        p0 = grid.transform.indexToWorld(ijk0)
+        p1 = grid.transform.indexToWorld(ijk1)
+        X = np.linspace(p0[0], p1[0], size[0])
+        Y = np.linspace(p0[1], p1[1], size[1])
+        Z = np.linspace(p0[2], p1[2], size[2])
+
+        A = np.zeros(size, dtype=np.float32)
+        grid.copyToArray(A, ijk=ijk0)
+
+        interpolator = interpolate.RegularGridInterpolator(
+            (X, Y, Z), A, bounds_error=False, fill_value=grid.background)
+
+        # num_voxels = size[0] * size[1] * size[2]
+        # print('mesh voxels = %d' % num_voxels)
+
         def f(p):
-            if not hasattr(p, 'info'):
-                return estimator(p)
-            grid = get_grid(p.info['step'])
-            a = np.zeros(p.info['shape'])
-            ijk = grid.transform.worldToIndex(p[0])
-            ijk = [int(round(x)) for x in ijk]
-            grid.copyToArray(a, ijk=ijk)
-            return a
+            e = estimator(p)
+            d = interpolator(p).reshape((-1, 1))
+            return np.where(e > grid.background, e, d)
 
         return f
